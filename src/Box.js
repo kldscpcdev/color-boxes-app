d1caae6c8d504d161a1d5a7eb89e1a8d55b2688a
kldscpcdev <80473306+kldscpcdev@users.noreply.github.com>
2021-08-29 22:14:13 -0700

    changed code around to make child stateful

--- src/Box.js
+++ src/Box.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
 import "./Box.css";
+import {choice} from "./helpers";
 
 class Box extends Component {
-    render () {
-        return <div className={`Box-box ${this.props.class}`}></div>
+    static defaultProps = {
+        allColors: ['#dcdae4','#d94a34','#5c0b0c','#e54b30','#3f5450','#a2816c','#f0c3c4','#e1e9d8','#dccc83','#b7ba9c']
+    }
+    constructor(props){
+        super(props);
+        this.state ={
+            boxColor: choice(this.props.allColors)
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+      colorSet() {
+          let newColor;
+          do { newColor = choice(this.props.allColors) } while (newColor === this.state.boxColor)
+            this.setState({boxColor: newColor} );
+    }
+
+    handleClick(e) {
+        this.colorSet();
+    }
+    render(){
+        return (<div className={`Box-box`} style={{backgroundColor: this.state.boxColor}} onClick={this.handleClick}></div>)
     }
 }
 
--- src/Picker.js
+++ src/Picker.js
@@ -2,22 +2,22 @@ import React, { Component } from 'react';
 import Box from "./Box";
 
 class Picker extends Component {
-    constructor(props){
-        super(props);
-        this.state ={
-            boxColorClass: `Box-box-cinnabar`
-        };
-        this.handleClick = this.handleClick.bind(this);
-    }
-    colorClick() {
-        this.setState({boxColorClass: `Box-box-sandal`} );
-    }
-
-    handleClick(e) {
-        this.colorClick();
-    }
     render () {
-        return <div onClick={this.handleClick}><Box class={this.state.boxColorClass}/></div>
+        const classes = [
+            {id: 1, class:'Box-box-mischka'},
+            {id: 2, class:'Box-box-valencia'},
+            {id: 3, class:'Box-box-maroon-oak'},
+            {id: 4, class:'Box-box-cinnabar'},
+            {id: 5, class:'Box-box-mineral-green'},
+            {id: 6, class:'Box-box-sandal'},
+            {id: 7, class:'Box-box-beauty-bush'},
+            {id: 8, class:'Box-box-periglacial-blue'},
+            {id: 9, class:'Box-box-calico'},
+            {id: 10, class:'Box-box-eagle'},
+        ]
+        return <div className="Box-container">
+           <Box />
+            </div>
     }
 }
 
